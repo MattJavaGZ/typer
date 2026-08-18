2c5d9d4e24c89bd6b35dab5a28f85b4f62ed6468
Matt <matek851@gmail.com>
2026-08-18 13:08:57 +0200

    Scheduled import update

--- src/main/java/matt/pas/typer/TyperApplication.java
+++ src/main/java/matt/pas/typer/TyperApplication.java
@@ -3,11 +3,12 @@ package matt.pas.typer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableAsync
 public class TyperApplication {
 
